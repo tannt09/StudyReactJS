ef8fc34cfe31015e0cd5c4a9c142d7883cb1f6ce
Nguyen Tan <tan.nguyen@widosoft.com>
2023-09-25 22:20:29 +0700

    add vitecompression

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import jsconfigPaths from 'vite-jsconfig-paths'
 import { fileURLToPath, URL } from 'url'
+import { viteCompression } from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react(), jsconfigPaths],
+  plugins: [
+    react(),
+    jsconfigPaths,
+    viteCompression({
+      verbose: false,
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+  ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  }
+  },
 })
