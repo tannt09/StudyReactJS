56911ee9be82207fcc1ea1c030d37729e04df051
widosofthackintosh <tan.nguyen@widosoft.com>
2023-09-25 17:55:03 +0700

    feat: test setup eslint 5

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import { SliderScreen } from '@/views'
 
 function App() {
-  console.log('Test eslint ::::1111 ')
+  console.log('Test eslint ::::2222 ')
   return (
     <>
       {/* <LoginScreen /> */}
